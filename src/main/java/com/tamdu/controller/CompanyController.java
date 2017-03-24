088130d82d8580b4c42b3df8c0cb2fb8335a6e6c
dusica <dusica@ballab.com>
2017-03-24 16:46:55 +0100

    new commit

---
+++ src/main/java/com/tamdu/controller/CompanyController.java
@@ -0,0 +1,26 @@
+package com.tamdu.controller;
+
+import com.tamdu.entity.Company;
+import com.tamdu.service.CompanyService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+
+/**
+ * Created by dusica on 24.3.17..
+ */
+
+@RestController
+public class CompanyController {
+
+    @Autowired
+    private CompanyService companyService;
+
+    @RequestMapping(value = "/companies", method = RequestMethod.GET)
+    public Collection<Company> getAllCompanies() {
+        return companyService.getAllCompanies();
+    }
+}
--- src/main/java/com/tamdu/controller/DashboardController.java
+++ src/main/java/com/tamdu/controller/DashboardController.java
@@ -1,13 +1,6 @@
 package com.tamdu.controller;
 
-import com.tamdu.entity.Company;
-import com.tamdu.service.CompanyService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Collection;
 
 /**
  * Created by dusica on 24.3.17..
@@ -16,11 +9,5 @@ import java.util.Collection;
 @RestController
 public class DashboardController {
 
-    @Autowired
-    private CompanyService companyService;
 
-    @RequestMapping(value = "/companies", method = RequestMethod.GET)
-    public Collection<Company> getAllCompanies() {
-        return companyService.getAllCompanies();
-    }
 }
