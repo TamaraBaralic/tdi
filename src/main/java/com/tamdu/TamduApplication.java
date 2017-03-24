58532866d0b5245084d56101d29f501cd71a1c7e
tamara <milosa@ballab.com>
2017-03-24 12:04:47 +0100

    first commit

---
+++ src/main/java/com/tamdu/TamduApplication.java
@@ -0,0 +1,12 @@
+package com.tamdu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TamduApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TamduApplication.class, args);
+	}
+}
