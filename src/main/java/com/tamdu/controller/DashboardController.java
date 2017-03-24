660a3968f6d2868efb121e9f6b6870f078351c4a
dusica <dusica@ballab.com>
2017-03-24 16:50:09 +0100

    new commit

--- src/main/java/com/tamdu/controller/DashboardController.java
+++ src/main/java/com/tamdu/controller/DashboardController.java
@@ -1,5 +1,6 @@
 package com.tamdu.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -9,5 +10,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DashboardController {
 
+    @RequestMapping("/")
+    public String index() {
+        return "index";
+    }
 
 }
