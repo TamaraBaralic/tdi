9a48549409c61857b0c0071001803ec14a372b5c
dusica <dusica@ballab.com>
2017-03-24 14:34:36 +0100

    creating repos and service

--- src/main/java/com/tamdu/controller/DashboardController.java
+++ src/main/java/com/tamdu/controller/DashboardController.java
@@ -1,7 +1,26 @@
 package com.tamdu.controller;
 
+import com.tamdu.entity.Company;
+import com.tamdu.service.CompanyService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+
 /**
  * Created by dusica on 24.3.17..
  */
+
+@RestController
+@RequestMapping("/")
 public class DashboardController {
+
+    @Autowired
+    private CompanyService companyService;
+
+    @RequestMapping("/getAllCompanies")
+    public Collection<Company> getAllCompanies() {
+        return null;
+    }
 }
