bafe1f965d0accb0a3637ad4316ed64c98a8a10c
dusica <dusica@ballab.com>
2017-03-29 11:24:30 +0200

    nesto

---
+++ build/resources/main/static/js/offer.js
@@ -0,0 +1,30 @@
+var user = {
+    init : function () {
+        this.fillTable();
+    },
+    fillTable : function () {
+
+        $.ajax({
+            url : '/offers/getAllOffers',
+            contentType: false,
+            processData: false,
+            success : function (dat) {
+                var offers = $("#allOffers").html("");
+
+                offers.append("<tr><th>Title</th><th>Description</th><tr>");
+
+                for(var i = 0; i < dat.length; i++) {
+                    var id = i + 1;
+                    offers.append("<tr><td><a href='/offers/" + id + "'>" + dat[i].title + "</a></td><td>" + dat[i].description +
+                        "</td></tr>");
+                }
+            },
+            error : function ()
+            { console.log("error"); }
+        });
+    }
+};
+
+$(function () {
+    user.init();
+});
